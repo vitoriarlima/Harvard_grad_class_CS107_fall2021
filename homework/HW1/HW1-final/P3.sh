91866704dcb74a06c75ee7ae9b6f1fb647bf7800
anavitor97 <anavitoria_rodrigueslima@fas.harvard.edu>
2021-09-14 19:15:20 -0400

    updating P3 after checking it

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -4,8 +4,8 @@
 grep -c '[[:digit:]]' apollo13.txt > apollo_out.txt  
 grep --help | grep '\-\-count'
 ls *.py | wc -l
-find . -type f ! -perm /o=w,o=r
-find . -type f,d ! -perm /o=w,o=r
+find . -type f ! -perm -o=w,o=r
+find . ! -perm -o=w,o=r
 
 
 
